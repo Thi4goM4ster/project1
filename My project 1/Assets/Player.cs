a5e072ccb9e71aa668339d578edab8382e3044ce
THIAGO MEDEIROS PAIVA <20221170150054@ifrn.local>
2023-10-23 13:42:48 -0300

    aula passada

--- My project 1/Assets/Player.cs
+++ My project 1/Assets/Player.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int jump = 7;
     private Rigidbody rb;
+
+    public bool noChao;
+
     void Start() {
         TryGetComponent(out rb);
 }
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag=="Floor") {
+            noChao = true;
+        }
+    }
     void Update() {
         float inputHorizontal = Input.GetAxis("Horizontal");
         float inputVertical = Input.GetAxis("Vertical");
         rb.AddForce(new Vector3(inputHorizontal,0,inputVertical) * velocidade);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && noChao){ 
+
+            rb.AddForce(Vector3.up*jump,ForceMode.Impulse);
+            noChao = false;
+        }
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
